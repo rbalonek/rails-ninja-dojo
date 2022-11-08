e11011c3d530f04c1baf8d70b1fe70e84fa2fc31
Robert Balonek <robertbalonek@gmail.com>
2022-11-08 09:31:24 -0800

    Migrate to fly.dev

--- client/src/services/api-config.js
+++ client/src/services/api-config.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const baseUrl = "https://rails-ninja.herokuapp.com/";
+const baseUrl = "https://rails-ninja-db.fly.dev/"
+// "https://rails-ninja.herokuapp.com/";
 
 const api = axios.create({
   baseURL: baseUrl,
